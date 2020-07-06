0221405bd95e02ffa295b4fbc75dcf121e626717
prem <prem.sherpa@codeimmersives.com>
2020-07-06 19:31:18 -0400

    one less

--- main.test.js
+++ main.test.js
@@ -1,6 +1,99 @@
 /******************
  * YOUR CODE HERE *
  ******************/
+const addAll = function (arr) {
+  let sum = 0;
+
+  for (const items of arr) {
+    sum += items;
+  }
+
+  return sum;
+};
+
+//2
+const countSpaces = function (sentence) {
+  let count = 0;
+  for (const items of sentence) {
+    if (items === " ") {
+      count++;
+    }
+  }
+
+  return count;
+};
+
+//3
+const countTrues = function (arr) {
+  let count = 0;
+  for (const items of arr) {
+    if (items === true) {
+      count++;
+    }
+  }
+
+  return count;
+};
+
+//4
+const makeFriendly = function (paragraph) {
+  let result = "";
+  for (const items of paragraph) {
+    if (items === ".") {
+      result += "!";
+    } else {
+      result += items;
+    }
+  }
+
+  return result;
+};
+
+//5
+const cubeAll = function(arr){
+  let newArr = [];
+  for ( const items of arr){
+    newArr.push(Math.pow(items, 3))
+  }
+newArr; 
+  return newArr;
+}
+
+//6
+const addNoises = function(arr){
+  let result = '';
+  const dogs = ["Fido", "Rolph", "Maisie"];
+  const cats = ["Garfield", "Heathcliff"];
+  const dinos = ["Barnie", "Sharp Tooth"];
+
+  for ( const items of arr){
+    if (items ==='Fido' || items === 'Rolph' || items === 'Maisie'){
+      result += `"${items} says "Woof!"",`
+    }
+    if (items === 'Garfield' || items ==='Heathcliff'){
+      result += `"${items} says "Meow!"",`
+    }
+    if (items === 'Barnie' || items ==='Sharp Tooth'){
+      result += `'"${items} says "ROWR."",`
+    }
+  }
+
+  result;
+  return result;
+}
+
+addNoises(['Fido','Rolph','Barnie']);
+
+
+
+
+
+
+
+
+
+
+
 
 
 
@@ -11,78 +104,75 @@
  * OUR CODE BELOW; DO NOT TOUCH! *
  *********************************/
 
-
-
-if (typeof addAll === 'undefined') {
+if (typeof addAll === "undefined") {
   addAll = undefined;
 }
 
-if (typeof countSpaces === 'undefined') {
+if (typeof countSpaces === "undefined") {
   countSpaces = undefined;
 }
 
-if (typeof countTrues === 'undefined') {
+if (typeof countTrues === "undefined") {
   countTrues = undefined;
 }
 
-if (typeof makeFriendly === 'undefined') {
+if (typeof makeFriendly === "undefined") {
   makeFriendly = undefined;
 }
 
-if (typeof cubeAll === 'undefined') {
+if (typeof cubeAll === "undefined") {
   cubeAll = undefined;
 }
 
-if (typeof addNoises === 'undefined') {
+if (typeof addNoises === "undefined") {
   addNoises = undefined;
 }
 
-
-describe('addAll', () => {
+describe("addAll", () => {
   it(`adds all the numbers in the array`, () => {
     const nums1 = [5, 3, 7, -11, 1003];
     const nums2 = [8, 1, 2, -53, -812];
 
     expect(addAll(nums1)).toBe(1007);
     expect(addAll(nums2)).toBe(-854);
-  })
+  });
 
   it(`isn't reassignable`, () => {
-    expect(() => addAll = 500).toThrow();
-  })
-})
+    expect(() => (addAll = 500)).toThrow();
+  });
+});
 
 describe(`countSpaces`, () => {
   it(`counts all the spaces in a multi-space string`, () => {
-    const sentence1 = 'well hello there my crumpet';
-    const sentence2 = 'strings 4 eva';
+    const sentence1 = "well hello there my crumpet";
+    const sentence2 = "strings 4 eva";
 
     expect(countSpaces(sentence1)).toBe(4);
     expect(countSpaces(sentence2)).toBe(2);
-  })
+  });
 
   it(`correctly identifies strings with no spaces`, () => {
-    const sentence1 = 'supercalifragilisticexpialidocious';
-    const sentence2 = 'antidisestablishmentarianism';
-    const sentence3 = 'pneunultramicroscopicsilicovulcanoconiosis';
+    const sentence1 = "supercalifragilisticexpialidocious";
+    const sentence2 = "antidisestablishmentarianism";
+    const sentence3 = "pneunultramicroscopicsilicovulcanoconiosis";
 
     expect(countSpaces(sentence1)).toBe(0);
     expect(countSpaces(sentence2)).toBe(0);
     expect(countSpaces(sentence3)).toBe(0);
-  })
+  });
 
   it(`doesn't count all non-letters as spaces`, () => {
-    const sentence1 = 'how now, brown cow';
-    const sentence2 = '@(*@ are you even talking about?';
+    const sentence1 = "how now, brown cow";
+    const sentence2 = "@(*@ are you even talking about?";
 
     expect(countSpaces(sentence1)).toBe(3);
     expect(countSpaces(sentence2)).toBe(5);
-  })
+  });
 
   it(`isn't reassignable`, () => {
-    expect(() => countSpaces = 500).toThrow();
-  })
-})
+    expect(() => (countSpaces = 500)).toThrow();
+  });
+});
 
 describe(`countTrues`, () => {
   it(`counts all the trues in a true-only array`, () => {
@@ -91,7 +181,7 @@ describe(`countTrues`, () => {
 
     expect(countTrues(bools1)).toBe(3);
     expect(countTrues(bools2)).toBe(5);
-  })
+  });
 
   it(`counts all the trues in an array of assorted booleans`, () => {
     const bools1 = [true, false, true, false];
@@ -99,32 +189,32 @@ describe(`countTrues`, () => {
 
     expect(countTrues(bools1)).toBe(2);
     expect(countTrues(bools2)).toBe(4);
-  })
+  });
 
   it(`doesn't confuse truthy values with true values`, () => {
-    const values1 = [true, 'hello', 3, true, []]
+    const values1 = [true, "hello", 3, true, []];
 
     expect(countTrues(values1)).toBe(2);
-  })
+  });
 
   it(`doesn't confuse the string 'true' with the boolean true`, () => {
-    const values1 = [true, 'true', 'true', true, 'true'];
-    const values2 = [true, 'true', 'true', true, true, 'true', 'true', true];
+    const values1 = [true, "true", "true", true, "true"];
+    const values2 = [true, "true", "true", true, true, "true", "true", true];
 
     expect(countTrues(values1)).toBe(2);
     expect(countTrues(values2)).toBe(4);
-  })
+  });
 
   it(`isn't reassignable`, () => {
-    expect(() => countTrues = 500).toThrow();
-  })
-})
+    expect(() => (countTrues = 500)).toThrow();
+  });
+});
 
-describe('makeFriendly', () => {
+describe("makeFriendly", () => {
   it(`given a paragraph, returns a paragraph where each sentence ends with an exclamation point instead of the period it held before.`, () => {
     const paragraph1 = `Do you see the story. Do you see anything. It seems to me I am trying to tell you a dream.`;
     const paragraph2 = `Like a running blaze on a plain, like a flash of lightning in the clouds. We live in the flicker.`;
-      const paragraph3 = `What is that feeling when you’re driving away from people and they recede on the plain till you see their specks dispersing. It’s the too-huge world vaulting us, and it’s good-bye. But we lean forward to the next crazy venture beneath the skies. Nothing behind me, everything ahead of me, as is ever so on the road.`;
+    const paragraph3 = `What is that feeling when you’re driving away from people and they recede on the plain till you see their specks dispersing. It’s the too-huge world vaulting us, and it’s good-bye. But we lean forward to the next crazy venture beneath the skies. Nothing behind me, everything ahead of me, as is ever so on the road.`;
 
     const friendly1 = `Do you see the story! Do you see anything! It seems to me I am trying to tell you a dream!`;
     const friendly2 = `Like a running blaze on a plain, like a flash of lightning in the clouds! We live in the flicker!`;
@@ -133,14 +223,14 @@ describe('makeFriendly', () => {
     expect(makeFriendly(paragraph1)).toEqual(friendly1);
     expect(makeFriendly(paragraph2)).toEqual(friendly2);
     expect(makeFriendly(paragraph3)).toEqual(friendly3);
-  })
+  });
 
   it(`isn't reassignable`, () => {
-    expect(() => makeFriendly = 500).toThrow();
-  })
-})
+    expect(() => (makeFriendly = 500)).toThrow();
+  });
+});
 
-describe('cubeAll', () => {
+describe("cubeAll", () => {
   it(`given an array of numbers, returns an array where each number has been cubed`, () => {
     const nums1 = [3, 2, 5];
     const cubes1 = [27, 8, 125];
@@ -149,7 +239,7 @@ describe('cubeAll', () => {
 
     expect(cubeAll(nums1)).toEqual(cubes1);
     expect(cubeAll(nums2)).toEqual(cubes2);
-  })
+  });
 
   it(`doesn't modify the original array`, () => {
     const nums1 = [3, 2, 5];
@@ -162,54 +252,53 @@ describe('cubeAll', () => {
 
     expect(nums1).toEqual(originalNums1);
     expect(nums2).toEqual(originalNums2);
-  })
+  });
 
   it(`isn't reassignable`, () => {
-    expect(() => cubeAll = 500).toThrow();
-  })
-})
+    expect(() => (cubeAll = 500)).toThrow();
+  });
+});
 
-describe('addNoises', () => {
-    it('adds the appropriate noises for each animal in the given array', () => {
-    const dogs = [
-      'Fido',
-      'Rolph',
-      'Maisie',
+describe("addNoises", () => {
+  it("adds the appropriate noises for each animal in the given array", () => {
+    const dogs = ["Fido", "Rolph", "Maisie"];
+
+    const cats = ["Garfield", "Heathcliff"];
+
+    const dinos = ["Barnie", "Sharp Tooth"];
+
+    const animals1 = ["Fido", "Garfield", "Sharp Tooth"];
+    const withNoises1 = [
+      'Fido says "Woof!"',
+      'Garfield says "Meow!"',
+      'Sharp Tooth says "ROWR."',
     ];
-
-    const cats = [
-      'Garfield',
-      'Heathcliff',
-    ]
-
-    const dinos = [
-      'Barnie',
-      'Sharp Tooth',
-    ]
-
-    const animals1 = ['Fido', 'Garfield', 'Sharp Tooth'];
-    const withNoises1 = ['Fido says "Woof!"', 'Garfield says "Meow!"', 'Sharp Tooth says "ROWR."'];
-    const animals2 = ['Rolph', 'Maisie', 'Barnie', 'Heathcliff'];
-    const withNoises2 = ['Rolph says "Woof!"', 'Maisie says "Woof!"', 'Barnie says "ROWR."', 'Heathcliff says "Meow!"'];
+    const animals2 = ["Rolph", "Maisie", "Barnie", "Heathcliff"];
+    const withNoises2 = [
+      'Rolph says "Woof!"',
+      'Maisie says "Woof!"',
+      'Barnie says "ROWR."',
+      'Heathcliff says "Meow!"',
+    ];
 
     expect(addNoises(animals1)).toEqual(withNoises1);
     expect(addNoises(animals2)).toEqual(withNoises2);
-  })
+  });
 
   it(`doesn't modify the original array`, () => {
-    const animals1 = ['Fido', 'Garfield', 'Sharp Tooth'];
-    const animals2 = ['Rolph', 'Maisie', 'Barnie', 'Heathcliff'];
-    const originalAnimals1 = ['Fido', 'Garfield', 'Sharp Tooth'];
-    const originalAnimals2 = ['Rolph', 'Maisie', 'Barnie', 'Heathcliff'];
+    const animals1 = ["Fido", "Garfield", "Sharp Tooth"];
+    const animals2 = ["Rolph", "Maisie", "Barnie", "Heathcliff"];
+    const originalAnimals1 = ["Fido", "Garfield", "Sharp Tooth"];
+    const originalAnimals2 = ["Rolph", "Maisie", "Barnie", "Heathcliff"];
 
     addNoises(animals1);
     addNoises(animals2);
 
     expect(animals1).toEqual(originalAnimals1);
     expect(animals2).toEqual(originalAnimals2);
-  })
+  });
 
   it(`isn't reassignable`, () => {
-    expect(() => addNoises = 500).toThrow();
-  })
-})
+    expect(() => (addNoises = 500)).toThrow();
+  });
+});
